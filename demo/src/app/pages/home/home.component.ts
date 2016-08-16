c409c5bb0850e62c506b641684837c492bbb6659
nixa <4dmitr@gmail.com>
2016-08-16 14:37:03 +0300

    feat(demo): theme styles

---
+++ demo/src/app/pages/home/home.component.ts
@@ -0,0 +1,17 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+
+@Component({
+  selector: 'home',
+  pipes: [],
+  directives: [],
+  encapsulation: ViewEncapsulation.None,
+  styles: [require('./home.scss')],
+  template: require('./home.html')
+})
+export class Home {
+
+  constructor() {
+  }
+
+}
