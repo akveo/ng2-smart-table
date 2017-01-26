d317c991cb09217f744ab414626ca62cb69b6e43
yari <yarlaz@gmail.com>
2017-01-26 09:47:00 +0100

    Added checkbox column type support and fixed multiselect bug when editing string values

--- demo/src/app/pages/demo/components/advanced-example-types.component.ts
+++ demo/src/app/pages/demo/components/advanced-example-types.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'advanced-example-types',
   template: `
-    <ng2-smart-table [settings]="settings" [source]="data" (onRowSelect)="change($event)"></ng2-smart-table>
+    <ng2-smart-table [settings]="settings" [source]="data"></ng2-smart-table>
   `
 })
 export class AdvancedExamplesTypesComponent {
@@ -13,67 +13,78 @@ export class AdvancedExamplesTypesComponent {
       id: 1,
       name: "Leanne Graham",
       username: "Bret",
-      email: "Sincere@april.biz"
+      email: "Sincere@april.biz",
+      passed: "No"
     },
     {
       id: 2,
       name: "Ervin Howell",
       username: "Antonette",
-      email: "Shanna@melissa.tv"
+      email: "Shanna@melissa.tv",
+      passed: "Yes"
     },
     {
       id: 3,
       name: "Clementine Bauch",
       username: "Samantha",
-      email: "Nathan@yesenia.net"
+      email: "Nathan@yesenia.net",
+      passed: "No"
     },
     {
       id: 4,
       name: "Patricia Lebsack",
       username: "Karianne",
-      email: "Julianne.OConner@kory.org"
+      email: "Julianne.OConner@kory.org",
+      passed: "Yes"
     },
     {
       id: 5,
       name: "Chelsey Dietrich",
       username: "Kamren",
-      email: "Lucio_Hettinger@annie.ca"
+      email: "Lucio_Hettinger@annie.ca",
+      passed: "Yes"
     },
     {
       id: 6,
       name: "Mrs. Dennis Schulist",
       username: "Leopoldo_Corkery",
-      email: "Karley_Dach@jasper.info"
+      email: "Karley_Dach@jasper.info",
+      passed: "No"
     },
     {
       id: 7,
       name: "Kurtis Weissnat",
       username: "Elwyn.Skiles",
-      email: "Telly.Hoeger@billy.biz"
+      email: "Telly.Hoeger@billy.biz",
+      passed: "Yes"
     },
     {
       id: 8,
       name: "Nicholas Runolfsdottir V",
       username: "Maxime_Nienow",
-      email: "Sherwood@rosamond.me"
+      email: "Sherwood@rosamond.me",
+      passed: "No"
     },
     {
       id: 9,
       name: "Glenna Reichert",
       username: "Delphine",
-      email: "Chaim_McDermott@dana.io"
+      email: "Chaim_McDermott@dana.io",
+      passed: "No"
     },
     {
       id: 10,
       name: "Clementina DuBuque",
       username: "Moriah.Stanton",
-      email: "Rey.Padberg@karina.biz"
+      email: "Rey.Padberg@karina.biz",
+      passed: "No"
     },
     {
       id: 11,
       name: "Nicholas DuBuque",
       username: "Nicholas.Stanton",
-      email: "Rey.Padberg@rosamond.biz"
+      email: "Rey.Padberg@rosamond.biz",
+      passed: "Yes"
     }
   ];
 
@@ -98,18 +109,22 @@ export class AdvancedExamplesTypesComponent {
         title: 'User Name',
         type: 'list',
         config: {
+          // when multiselect is possible, the source list (usernamne field) should be of type array
           multiple: true,
           list: [{value: 'Antonette', title: 'Antonette'}, {value: 'Bret', title: 'Bret'}, {value: 'Samantha', title: 'Samantha'}]
         }
       },
       email: {
         title: 'Email',
-        type: 'checkbox'
+      },
+      passed: {
+        title: 'Passed',
+        type: 'checkbox',
+        config: {
+          true: 'Yes',
+          false: 'No'
+        }
       }
     }
   };
-
-  change(event) {
-    console.log('cambiato', event);
-  }
 }
--- src/ng2-smart-table/components/multi-select/multi-select.service.ts
+++ src/ng2-smart-table/components/multi-select/multi-select.service.ts
@@ -3,8 +3,12 @@ import { Injectable, ElementRef } from '@angular/core';
 @Injectable()
 export class MultiSelectService {
 
-  simpleSelect(targetDOMElement: ElementRef, disabled: boolean, options: Array<any>, selectedValue: string, multiple: boolean): void {
+  simpleSelect(targetDOMElement: ElementRef, disabled: boolean, options: Array<any>, selectedValue: string | Array<string>, multiple: boolean): void {
     let items = this.refactorOptions(options);
+
+    // if multiple select is possible, selectedValue must be an array of IDs, if not, convert to array
+    if (multiple && typeof(selectedValue) == 'string')
+      selectedValue = [selectedValue];
 
     let config = {
       allowClear: true,
