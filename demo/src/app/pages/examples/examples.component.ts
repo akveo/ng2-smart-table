88402e920eec4e07dd7f48401ed103ca239007ea
yari <yarlaz@gmail.com>
2017-03-10 15:16:20 +0100

    fix(documentation): merge errors

--- demo/src/app/pages/examples/examples.component.ts
+++ demo/src/app/pages/examples/examples.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'examples',
-<<<<<<< HEAD
     templateUrl: 'examples.component.html'
-=======
-    templateUrl: 'examples.html'
->>>>>>> ae44008764f190f77e4c976bfa25f170d3b05b1f
 })
 export class ExamplesComponent {
 }
--- demo/src/app/pages/pages.module.ts
+++ demo/src/app/pages/pages.module.ts
@@ -9,29 +9,11 @@ import { SharedModule } from '../shared/shared.module';
 import { DemoComponent } from './demo';
 import { DocumentationComponent } from './documentation';
 import { HomeComponent } from './home';
-import { HeaderComponent } from '../components';
-import { ExamplesComponent,
-         FilterExamplesComponent,
-         DefaultExampleComponent,
-         ServerExamplesComponent,
-         CustomViewEditExamplesComponent,
-         VariousExamplesComponent } from './examples';
 import { Ng2SmartTableModule } from '../../../../src/ng2-smart-table.module';
 import { ScrollPositionDirective } from '../theme/directives/scrollPosition.directive';
 
 import { BasicExampleComponent } from './demo/components/basic-example.component';
 import { BasicExampleDataComponent } from './demo/components/basic-example-data.component';
-
-import { BasicExampleSourceComponent } from './examples/tables/basic-example-source.component';
-import { BasicExampleLoadComponent } from './examples/tables/basic-example-load.component';
-import { BasicExampleMultiSelectComponent } from './examples/tables/basic-example-multi-select.component';
-import { AdvancedExampleConfirmComponent } from './examples/tables/advanced-example-confirm.component';
-import { AdvancedExampleServerComponent } from './examples/tables/advanced-example-server.component';
-import { AdvancedExamplesTypesComponent } from './examples/tables/advanced-example-types.component';
-import { AdvancedExamplesCustomEditorComponent } from './examples/tables/advanced-example-custom-editor.component';
-import { AdvancedExampleFiltersComponent } from './examples/tables/advanced-example-filters.component';
-import { CustomEditorComponent } from './examples/tables/custom-editor.component';
-import { CustomRenderComponent } from './examples/tables/custom-render.component';
 
 @NgModule({
   imports: [
@@ -43,16 +25,9 @@ import { CustomRenderComponent } from './examples/tables/custom-render.component
     routing,
   ],
   declarations: [
-    HeaderComponent,
     DemoComponent,
     DocumentationComponent,
     HomeComponent,
-    ExamplesComponent,
-    DefaultExampleComponent,
-    FilterExamplesComponent,
-    ServerExamplesComponent,
-    CustomViewEditExamplesComponent,
-    VariousExamplesComponent,
     BasicExampleComponent,
     BasicExampleDataComponent,
     ScrollPositionDirective,
