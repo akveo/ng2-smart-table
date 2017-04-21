6fa37fd5358394efe230b9a57764a87c69c52c81
gaurav8547 <gaurav8547@yahoo.com>
2017-04-20 21:36:59 -0400

    c 3

--- src/app/shared/components/basic-example/basic-example-data.component.ts
+++ src/app/shared/components/basic-example/basic-example-data.component.ts
@@ -29,7 +29,7 @@ export class BasicExampleDataComponent {
     {
       id: 1,
       name: 'Leanne Graham',
-      username: '',
+      username: 'Bret',
       email: 'Sincere@april.biz',
     },
     {
