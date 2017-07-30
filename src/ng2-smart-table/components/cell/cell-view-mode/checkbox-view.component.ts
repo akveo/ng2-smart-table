3cd80e502ddcb3788bbd54ef27b8f59677b95fb8
Ragnarok <tzvikakoifman@gmail.com>
2017-07-30 19:27:55 +0300

    added support for checkbox type render

---
+++ src/ng2-smart-table/components/cell/cell-view-mode/checkbox-view.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Cell } from '../../../lib/data-set/cell';
+
+@Component({
+  selector: 'checkbox-view-component',
+  template: `    
+    <input type="checkbox" 
+           [ngClass]="inputClass" 
+           class="form-control"
+           [checked] = "renderValue"
+           onclick="return false;"/>
+  `,
+})
+export class CheckboxViewComponent implements OnInit {
+
+  @Input() cell: Cell;
+  renderValue;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.renderValue = (this.cell.getValue() === true) ? true : false;
+  }
+}
