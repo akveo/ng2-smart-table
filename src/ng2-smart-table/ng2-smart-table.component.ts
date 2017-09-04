1a114768c22fdc32b8301eb548afda8215136a27
neal.yip <neal.yip@webssup.com>
2017-09-04 15:12:32 +0800

    Manually set page

--- src/ng2-smart-table/ng2-smart-table.component.ts
+++ src/ng2-smart-table/ng2-smart-table.component.ts
@@ -15,6 +15,7 @@ export class Ng2SmartTableComponent implements OnChanges {
 
   @Input() source: any;
   @Input() settings: Object = {};
+  @Input() page: number;
 
   @Output() rowSelect = new EventEmitter<any>();
   @Output() userRowSelect = new EventEmitter<any>();
