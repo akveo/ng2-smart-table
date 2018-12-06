47102fc245ca6725013f4ceea6412c8567ec115c
Tan Guanghua <guanghua.tan@desay-svautomotive.com>
2018-12-06 09:40:21 +0800

    fix: rm skip(1) in input-filter

--- src/ng2-smart-table/components/filter/filter-types/input-filter.component.ts
+++ src/ng2-smart-table/components/filter/filter-types/input-filter.component.ts
@@ -29,7 +29,6 @@ export class InputFilterComponent extends DefaultFilter implements OnInit {
     }
     this.inputControl.valueChanges
       .pipe(
-        skip(1),
         distinctUntilChanged(),
         debounceTime(this.delay),
       )
