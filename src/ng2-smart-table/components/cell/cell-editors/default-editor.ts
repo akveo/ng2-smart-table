019a81d40a63b9c87b26ce8835e74c6fbec61b68
Myles Fang <myles.fang@imsure.life>
2019-03-27 17:28:42 +0800

    Add disableEnterKeySave option for editor.config

--- src/ng2-smart-table/components/cell/cell-editors/default-editor.ts
+++ src/ng2-smart-table/components/cell/cell-editors/default-editor.ts
@@ -9,6 +9,10 @@ export class DefaultEditor implements Editor {
   @Output() onStopEditing = new EventEmitter<any>();
   @Output() onEdited = new EventEmitter<any>();
   @Output() onClick = new EventEmitter<any>();
+
+  get disableEnterKeySave(): boolean {
+    return this.cell.getColumn().getConfig() && this.cell.getColumn().getConfig().disableEnterKeySave;
+  }
 }
 
 export interface Editor {
