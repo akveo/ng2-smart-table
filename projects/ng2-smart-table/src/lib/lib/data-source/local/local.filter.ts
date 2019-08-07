e9e502d420866f372874e3584d2dbb5db9ba7621
Singh Hemant <hemant.singh@nokia.com>
2019-08-07 16:33:19 +0530

    Updated local.filter.ts to fix null cell value in nested data

--- projects/ng2-smart-table/src/lib/lib/data-source/local/local.filter.ts
+++ projects/ng2-smart-table/src/lib/lib/data-source/local/local.filter.ts
@@ -4,12 +4,55 @@ export function filterValues(value: string, search: string) {
 
 export class LocalFilter {
 
+  /*
+  new Logic start
+  */
+  
+
+
+  protected static FILTER = (value: string, search: string) => {
+    return value.toString().toLowerCase().includes(search.toString().toLowerCase());
+  }
+
+  static filter(data: Array<any>, field: string, search: string, customFilter?: Function): Array<any> {
+    const filter: Function = customFilter ? customFilter : this.FILTER;
+
+    return data.filter((el) => {
+      // Holds the data
+      let data: any = el;
+
+      // Split the property string
+      const propertyList: string[] = field.split(".");
+
+      // Access inner properties
+      for(const property of propertyList) {
+        data = data[property];
+      }
+
+      const value = typeof data === 'undefined' || data === null ? '' : data;
+      return filter.call(null, value, search);
+    });
+  }
+
+  /*
+    new logic end
+  */
+
+  /*
+  Original Start
+  
+
   static filter(data: Array<any>, field: string, search: string, customFilter?: Function): Array<any> {
     const filter: Function = customFilter ? customFilter : filterValues;
 
     return data.filter((el) => {
       const value = typeof el[field] === 'undefined' || el[field] === null ? '' : el[field];
       return filter.call(null, value, search);
+      //return filter.call(null, el, search);
     });
   }
+  
+  /*
+  Original end
+  */
 }
