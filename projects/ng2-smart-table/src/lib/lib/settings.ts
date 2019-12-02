93fa8f6ffd4a77e10537194e3c155449bb1cd459
rafael.lucini <rafael.lucini@viasoft.com.br>
2019-12-02 10:27:17 -0300

    Typing Settings

--- projects/ng2-smart-table/src/lib/lib/settings.ts
+++ projects/ng2-smart-table/src/lib/lib/settings.ts
@@ -1,18 +1,75 @@
-import {Column} from "./data-set/column";
-
 export interface Settings {
   columns?: Columns,
   mode?: 'external' | 'inline';
   hideHeader?: boolean;
   hideSubHeader?: boolean;
   noDataMessage?: string;
-  attr?: object;
-  actions?: object;
-  edit?: object;
-  add?: object;
-  delete?: object;
+  attr?: Attribute;
+  actions?: Actions;
+  edit?: EditAction;
+  add?: AddAction;
+  delete?: DeleteAction;
+  pager?: Pager;
+  rowClassFunction?: Function
 }
 
 export interface Columns {
-  [name: string] : object
+  [key: string]: Column;
+}
+
+export interface Column {
+  title?: string;
+  type?: 'text' | 'html' | 'custom';
+  class?: string;
+  width?: string;
+  editable?: boolean;
+  sort?: boolean;
+  sortDirection?: 'asc' | 'desc';
+  defaultSortDirection?: string;
+  editor?: { type: string, config: any, component: any };
+  filter?: { type: string, config: any, component: any } | boolean;
+  renderComponent?: any;
+  compareFunction?: Function;
+  valuePrepareFunction?: Function;
+  filterFunction?: Function;
+  onComponentInitFunction?: Function;
+}
+
+export interface Attribute {
+    id?: string;
+    class?: string;
+}
+
+export interface Actions {
+  columnTitle?: string;
+  add?: boolean;
+  edit?: boolean;
+  delete?: boolean;
+  position?: 'left' | 'right';
+}
+
+export interface AddAction {
+  inputClass?: string;
+  addButtonContent?: string;
+  createButtonContent?: string;
+  cancelButtonContent?: string;
+  confirmCreate?: boolean;
+}
+
+export interface EditAction {
+  inputClass?: string;
+  editButtonContent?: string;
+  saveButtonContent?: string;
+  cancelButtonContent?: string;
+  confirmSave?: boolean;
+}
+
+export interface DeleteAction {
+  deleteButtonContent?: string;
+  confirmDelete?: boolean;
+}
+
+export interface Pager {
+  display?: string;
+  perPage?: number;
 }
