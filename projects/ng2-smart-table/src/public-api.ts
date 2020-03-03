01f0c43e93037bed533fea89051caa60fc28b767
Sergey Andrievskiy <st.17028@gmail.com>
2020-03-03 18:07:34 +0300

    fix: add smart table component to public api

--- projects/ng2-smart-table/src/public-api.ts
+++ projects/ng2-smart-table/src/public-api.ts
@@ -5,3 +5,4 @@ export { DefaultFilter, Filter } from './lib/components/filter/filter-types/defa
 export { Cell } from './lib/lib/data-set/cell';
 export { LocalDataSource } from './lib/lib/data-source/local/local.data-source';
 export { ServerDataSource } from './lib/lib/data-source/server/server.data-source';
+export * from './lib/ng2-smart-table.component';
