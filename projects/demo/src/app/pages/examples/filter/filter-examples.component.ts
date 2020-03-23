8e98812f7960a4c12bd5d94d5fe724d04cc85d3b
Sergey Andrievskiy <st.17028@gmail.com>
2020-03-23 13:25:50 +0300

    fix(demo): update imports to use default export

--- projects/demo/src/app/pages/examples/filter/filter-examples.component.ts
+++ projects/demo/src/app/pages/examples/filter/filter-examples.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 export class FilterExamplesComponent {
 
   snippets = {
-    sourceRequire: require('raw-loader!../snippets/source-require.md'),
-    createSource: require('raw-loader!../snippets/create-source.md'),
-    sourceTemplate: require('raw-loader!../snippets/source-template.md'),
-    search: require('raw-loader!../snippets/search.md'),
-    searchTable: require('raw-loader!../snippets/search-table.md'),
-    sourceFull: require('raw-loader!../snippets/source-full.md'),
-    hideFilters: require('raw-loader!../snippets/hide-filters.md'),
+    sourceRequire: require('raw-loader!../snippets/source-require.md').default,
+    createSource: require('raw-loader!../snippets/create-source.md').default,
+    sourceTemplate: require('raw-loader!../snippets/source-template.md').default,
+    search: require('raw-loader!../snippets/search.md').default,
+    searchTable: require('raw-loader!../snippets/search-table.md').default,
+    sourceFull: require('raw-loader!../snippets/source-full.md').default,
+    hideFilters: require('raw-loader!../snippets/hide-filters.md').default,
   };
 
 }
