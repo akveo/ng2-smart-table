8fa539adb92e6669ad2bc6eab2b8ba4a82604d5f
Sergey Andrievskiy <st.17028@gmail.com>
2020-10-29 15:54:32 +0300

    style: add missing semicolon

--- projects/ng2-smart-table/src/lib/components/thead/rows/thead-titles-row.component.ts
+++ projects/ng2-smart-table/src/lib/components/thead/rows/thead-titles-row.component.ts
@@ -44,6 +44,6 @@ export class TheadTitlesRowComponent implements OnChanges {
   }
 
   getVisibleColumns(columns: Array<Column>): Array<Column> {
-    return (columns || []).filter((column: Column) => !column.hide)
+    return (columns || []).filter((column: Column) => !column.hide);
   }
 }
